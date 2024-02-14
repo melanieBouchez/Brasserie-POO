08273a6cb61ae18696e4fc2e122b3594e1cc2765
Mélanie Bouchez <melaniebouchez@hotmail.com>
2024-02-14 19:36:10 +0100

    Ajoutez des fichiers projet.

---
+++ Brasserie/Model/Restaurant/People/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brasserie.Model.Restaurant.People
+{
+
+    public class Person
+    {
+        private int _id;
+        private string _lastName;
+        private string _firstName;
+        private bool _gender;
+        private string _email;
+        private string _mobilePhoneNumber;
+
+
+        public Person(int id, string lastName = "nom", string firstName = "prenom", bool gender = true, string email = "", string mobilePhoneNumber = "")
+        {
+            _id = id;
+            _lastName = lastName;
+            _firstName = firstName;
+            _gender = gender;
+            _email = email;
+            _mobilePhoneNumber = mobilePhoneNumber;
+        }
+
+        public Person() { }
+    }
+}
---
+++ Brasserie/View/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Brasserie.Model.Restaurant.People;
+
+namespace Brasserie.View
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            EntryCount.Text = count.ToString();
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void buttonTestCreateFirstPersons_Clicked(object sender, EventArgs e)
+        {
+            Person firstPerson = new Person(id: 1, lastName: "Beumier", firstName: "Damien", gender: true, email: "dambeumier@gmail.com", mobilePhoneNumber: "0489142293");
+            Person secondPerson = new Person(id: 2, lastName: "Deroisin", firstName: "Sophie", gender: false, email: "sophiederoisin@gmail.com", mobilePhoneNumber: "0473121314");
+            Person ThirdPerson = new Person(3, "Jandrin", "Marc", true, "jandrinmarc@gmail.com", mobilePhoneNumber: "0485556678");
+            Person FourthPerson = new Person(4, "Lupant", "Sebastien");
+            Person FifthPerson = new Person();
+            //Person TestPerson = new Person( lastName: "Lupant", firstName: "Sebastien");
+
+            Person p;
+
+            p = new Person(6, "Tardif","Jean");
+
+            secondPerson._email = "soderoisin@ecole.be";
+ 
+        }
+
+    }
+
+}
